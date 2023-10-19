8a39fd9e696fa5976e9c4d6f64d5d7887381de6b
오원준 <kbw0309@douzone.com>
2023-10-19 11:30:50 +0900

    번들링 로직 추가

--- src/preview/Preview.js
+++ src/preview/Preview.js
@@ -21,68 +21,133 @@ const Preview = (props) => {
         </html>
     `)
 
-    useEffect(() => {
-        // 간단한 위험 패턴 찾기
-        const isUnsafe = /alert|localStorage|sessionStorage|cookie|document\.write/.test(compiledCode);
-        if (isUnsafe) {
-            debugger
-        }
+    // useEffect(() => {
+    //     // 간단한 위험 패턴 찾기
+    //     const isUnsafe = /alert|localStorage|sessionStorage|cookie|document\.write/.test(compiledCode);
+    //     if (isUnsafe) {
+    //         debugger
+    //     }
+    //
+    //     // Babel을 사용하여 코드 변환
+    //     let transformedCode;
+    //     try {
+    //         transformedCode = window.Babel.transform(compiledCode, {
+    //             presets: ['react']
+    //         }).code.replace(/export /g, "");
+    //     } catch (error) {
+    //         debugger
+    //     }
+    //
+    //     let sanitizedCode = transformedCode.split('\n').filter(line => !line.trim().startsWith('default')).join('\n');
+    //
+    //     sanitizedCode = sanitizedCode
+    //         .replace(/import React from 'react';/g, 'const React = window.React;')
+    //         .replace(/import {([^}]+)} from 'react';/g, (match, p1) => {
+    //             // named exports를 분리 (useState, useEffect, ...)
+    //             const names = p1.split(',').map(name => name.trim());
+    //             return names.map(name => `const ${name} = window.React.${name};`).join('\n');
+    //         });
+    //
+    //
+    //     let match = sanitizedCode.match(/function (\w+)|class (\w+)|const (\w+) = \(.*\)\s*=>\s*{?\s*return/);
+    //     let componentName = match && (match[1] || match[2] || match[3]);
+    //
+    //     if (!componentName) {
+    //         console.error("컴포넌트 이름을 찾을 수 없습니다.");
+    //         return;
+    //     }
+    //
+    //     const htmlCode = `
+    //         <!DOCTYPE html>
+    //         <html lang="en">
+    //         <head>
+    //             <meta charset="UTF-8">
+    //             <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    //             <script crossorigin src="https://unpkg.com/react@17/umd/react.development.js"></script>
+    //             <script crossorigin src="https://unpkg.com/react-dom@17/umd/react-dom.development.js"></script>
+    //             <title>Preview</title>
+    //         </head>
+    //         <body>
+    //             <div id="root"></div>
+    //             <script>
+    //                 ${sanitizedCode}
+    //                 window.${componentName} = ${componentName};
+    //                 ReactDOM.render(React.createElement(window.${componentName}), document.getElementById('root'));
+    //             </script>
+    //         </body>
+    //         </html>
+    //     `;
+    //
+    //     const blob = new Blob([htmlCode], {type: 'text/html'});
+    //     const blobURL = URL.createObjectURL(blob);
+    //
+    //     setCode(blobURL);
+    // }, [compiledCode])
 
-        // Babel을 사용하여 코드 변환
-        let transformedCode;
+
+    const sendCodeToServer=async (code)=> {
         try {
-            transformedCode = window.Babel.transform(compiledCode, {
-                presets: ['react']
-            }).code.replace(/export /g, "");
-        } catch (error) {
-            debugger
-        }
-
-        let sanitizedCode = transformedCode.split('\n').filter(line => !line.trim().startsWith('default')).join('\n');
-
-        sanitizedCode = sanitizedCode
-            .replace(/import React from 'react';/g, 'const React = window.React;')
-            .replace(/import {([^}]+)} from 'react';/g, (match, p1) => {
-                // named exports를 분리 (useState, useEffect, ...)
-                const names = p1.split(',').map(name => name.trim());
-                return names.map(name => `const ${name} = window.React.${name};`).join('\n');
+            // 서버의 /bundle 엔드포인트로 POST 요청을 보냅니다.
+            const response = await fetch('http://localhost:3001/bundle', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({code: code})
             });
 
+            if (!response.ok) {
+                throw new Error(`HTTP error! Status: ${response.status}`);
+            }
 
-        let match = sanitizedCode.match(/function (\w+)|class (\w+)|const (\w+) = \(.*\)\s*=>\s*{?\s*return/);
-        let componentName = match && (match[1] || match[2] || match[3]);
-
-        if (!componentName) {
-            console.error("컴포넌트 이름을 찾을 수 없습니다.");
-            return;
+            const bundledCode = await response.text();
+            return bundledCode;
+        } catch (error) {
+            console.error("Error sending code to server:", error);
+            return null;
         }
+    }
 
-        const htmlCode = `
-            <!DOCTYPE html>
-            <html lang="en">
-            <head>
-                <meta charset="UTF-8">
-                <meta name="viewport" content="width=device-width, initial-scale=1.0">
-                <script crossorigin src="https://unpkg.com/react@17/umd/react.development.js"></script>
-                <script crossorigin src="https://unpkg.com/react-dom@17/umd/react-dom.development.js"></script>
-                <title>Preview</title>
-            </head>
-            <body>
-                <div id="root"></div>
-                <script>
-                    ${sanitizedCode}
-                    window.${componentName} = ${componentName};
-                    ReactDOM.render(React.createElement(window.${componentName}), document.getElementById('root'));
-                </script>
-            </body>
-            </html>
-        `;
+    useEffect(() => {
+        if (compiledCode !== "" && compiledCode !== null && compiledCode !== undefined) {
+            let match = compiledCode.match(/function (\w+)|class (\w+)|const (\w+) = \(.*\)\s*=>\s*{?\s*return/);
+            let componentName = match && (match[1] || match[2] || match[3]);
 
-        const blob = new Blob([htmlCode], {type: 'text/html'});
-        const blobURL = URL.createObjectURL(blob);
+            if (!componentName) {
+                console.error("컴포넌트 이름을 찾을 수 없습니다.");
+                return;
+            }
 
-        setCode(blobURL);
-    }, [compiledCode])
+            const completeCode = `
+                                    ${compiledCode}
+                                    window.${componentName} = ${componentName};
+                                     ReactDOM.render(React.createElement(window.${componentName}), document.getElementById('root'));
+                                `;
+
+
+            sendCodeToServer(completeCode).then(bundledCode => {
+                if (bundledCode) {
+                    const wrappedHTML = `
+                    <html>
+                    <head>
+                        <script src="https://unpkg.com/react@17/umd/react.development.js"></script>
+                        <script src="https://unpkg.com/react-dom@17/umd/react-dom.development.js"></script>
+                        <script src="https://unpkg.com/react@17/umd/react.production.min.js"></script>
+                        <script src="https://unpkg.com/react-dom@17/umd/react-dom.production.min.js"></script>
+                    </head>
+                    <body>
+                        <div id="root"></div>
+                        <script>${bundledCode}</script>
+                    </body>
+                    </html>
+                `;
+                    const blob = new Blob([wrappedHTML], { type: 'text/html' });
+                    const blobURL = URL.createObjectURL(blob);
+                    setCode(blobURL)
+                }
+            });
+        }
+    },[compiledCode])
 
 
     return (
--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const MonacoWebpackPlugin = require('monaco-editor-webpack-plugin');
 const path = require('path');
 
 module.exports = {
-  mode: 'production',
+  mode: 'development',
     entry: './temp-code.js',  // 임시 파일 위치
     output: {
         path: path.resolve(__dirname, 'dist'),
@@ -27,5 +27,9 @@ module.exports = {
       // available options are documented at https://github.com/microsoft/monaco-editor/blob/main/webpack-plugin/README.md#options
       languages: ['json']
     })
-  ]
+  ],
+    externals: {
+    "react": "React",      // `import React from 'react'`는 전역 변수 React를 사용합니다.
+    "react-dom": "ReactDOM" // `import ReactDOM from 'react-dom'`는 전역 변수 ReactDOM를 사용합니다.
+  }
 };
